fb536e4d5f19355775f86fda5a2f5d35b41dc0fd
Iraê <irae@irae.pro.br>
2010-05-11 22:25:39 -0300

    Preparing for canvas redraw, separating from setup. Tables can have multiple THs.

--- visualize.jQuery.js
+++ visualize.jQuery.js
@@ -49,7 +49,7 @@ $.fn.visualize = function(options, container){
 				dataGroups: function(){
 					var dataGroups = [];
 					if(o.parseDirection == 'x'){
-						self.find('tr:gt(0)').each(function(i){
+						self.find('tbody tr').each(function(i){
 							dataGroups[i] = {};
 							dataGroups[i].points = [];
 							dataGroups[i].color = colors[i];
@@ -60,13 +60,13 @@ $.fn.visualize = function(options, container){
 						});
 					}
 					else {
-						var cols = self.find('tr:eq(1) td').size();
+						var cols = self.find('tbody tr:eq(0) td').size();
 						for(var i=0; i<cols; i++){
 							dataGroups[i] = {};
 							dataGroups[i].points = [];
 							dataGroups[i].color = colors[i];
 							if(textColors[i]){ dataGroups[i].textColor = textColors[i]; }
-							self.find('tr:gt(0)').each(function(){
+							self.find('tbody tr').each(function(){
 								dataGroups[i].points.push( $(this).find('td').eq(i).text()*1 );
 							});
 						};
@@ -149,12 +149,12 @@ $.fn.visualize = function(options, container){
 				xLabels: function(){
 					var xLabels = [];
 					if(o.parseDirection == 'x'){
-						self.find('tr:eq(0) th').each(function(){
+						self.find('thead tr:eq(0) th').each(function(){
 							xLabels.push($(this).html());
 						});
 					}
 					else {
-						self.find('tr:gt(0) th').each(function(){
+						self.find('tbody tr th:first-child').each(function(){
 							xLabels.push($(this).html());
 						});
 					}
@@ -182,21 +182,30 @@ $.fn.visualize = function(options, container){
 		};
 		
 		
-		//function to create a chart
-		var createChart = {
-			pie: function(){	
-				
-				canvasContain.addClass('visualize-pie');
-				
-				if(o.pieLabelPos == 'outside'){ canvasContain.addClass('visualize-pie-outside'); }	
-						
+		var charts = {};
+		
+		
+		charts.pie = {
+			setup: function() {
+				charts.pie.draw(true);
+			},
+			draw: function(drawHtml){	
+
 				var centerx = Math.round(canvas.width()/2);
 				var centery = Math.round(canvas.height()/2);
 				var radius = centery - o.pieMargin;				
 				var counter = 0.0;
-				var toRad = function(integer){ return (Math.PI/180)*integer; };
-				var labels = $('<ul class="visualize-labels"></ul>')
-					.insertAfter(canvas);
+
+				if(drawHtml) {
+					canvasContain.addClass('visualize-pie');
+
+					if(o.pieLabelPos == 'outside'){ canvasContain.addClass('visualize-pie-outside'); }	
+
+					var toRad = function(integer){ return (Math.PI/180)*integer; };
+					var labels = $('<ul class="visualize-labels"></ul>')
+						.insertAfter(canvas);
+				}
+
 
 				//draw the pie pieces
 				$.each(memberTotals, function(i){
@@ -214,139 +223,169 @@ $.fn.visualize = function(options, container){
 			        ctx.fillStyle = dataGroups[i].color;
 			        ctx.fill();
 			        // draw labels
-			       	var sliceMiddle = (counter + fraction/2);
-			       	var distance = o.pieLabelPos == 'inside' ? radius/1.5 : radius +  radius / 5;
-			        var labelx = Math.round(centerx + Math.sin(sliceMiddle * Math.PI * 2) * (distance));
-			        var labely = Math.round(centery - Math.cos(sliceMiddle * Math.PI * 2) * (distance));
-			        var leftRight = (labelx > centerx) ? 'right' : 'left';
-			        var topBottom = (labely > centery) ? 'bottom' : 'top';
-			        var labeltext = $('<span class="visualize-label">' + Math.round(fraction*100) + '%</span>')
-			        	.css(leftRight, 0)
-			        	.css(topBottom, 0);
-			        var label = $('<li class="visualize-label-pos"></li>')
-			       			.appendTo(labels)
-			        		.css({left: labelx, top: labely})
-			        		.append(labeltext);	
-			        labeltext
-			        	.css('font-size', radius / 8)
-			        	.css('margin-'+leftRight, -labeltext.width()/2)
-			        	.css('margin-'+topBottom, -labeltext.outerHeight()/2);
-			        	
-			        if(dataGroups[i].textColor){ labeltext.css('color', dataGroups[i].textColor); }	
-					if(o.interaction) {
-						interactionPoints.push({tableCords:[i,0],canvasCords:[labelx,labely]})
+					if(drawHtml) {
+				       	var sliceMiddle = (counter + fraction/2);
+				       	var distance = o.pieLabelPos == 'inside' ? radius/1.5 : radius +  radius / 5;
+				        var labelx = Math.round(centerx + Math.sin(sliceMiddle * Math.PI * 2) * (distance));
+				        var labely = Math.round(centery - Math.cos(sliceMiddle * Math.PI * 2) * (distance));
+				        var leftRight = (labelx > centerx) ? 'right' : 'left';
+				        var topBottom = (labely > centery) ? 'bottom' : 'top';
+				        var labeltext = $('<span class="visualize-label">' + Math.round(fraction*100) + '%</span>')
+				        	.css(leftRight, 0)
+				        	.css(topBottom, 0);
+				        var label = $('<li class="visualize-label-pos"></li>')
+				       			.appendTo(labels)
+				        		.css({left: labelx, top: labely})
+				        		.append(labeltext);	
+				        labeltext
+				        	.css('font-size', radius / 8)
+				        	.css('margin-'+leftRight, -labeltext.width()/2)
+				        	.css('margin-'+topBottom, -labeltext.outerHeight()/2);
+
+				        if(dataGroups[i].textColor){ labeltext.css('color', dataGroups[i].textColor); }
+
+						if(o.interaction) {
+							interactionPoints.push({tableCords:[i,0],canvasCords:[labelx,labely]})
+						}
 					}
+
 			      	counter+=fraction;
 				});
+			}
+		};
+		
+		(function(){
+			
+			var yScale,xInterval;
+
+			charts.line = {
+
+				setup: function(area){
+
+					if(area){ canvasContain.addClass('visualize-area'); }
+					else{ canvasContain.addClass('visualize-line'); }
+
+					//write X labels
+					xInterval = canvas.width() / (xLabels.length -1);
+					var xlabelsUL = $('<ul class="visualize-labels-x"></ul>')
+						.width(canvas.width())
+						.height(canvas.height())
+						.insertBefore(canvas);
+
+					$.each(xLabels, function(i){ 
+						var thisLi = $('<li><span>'+this+'</span></li>')
+							.prepend('<span class="line" />')
+							.css('left', xInterval * i)
+							.appendTo(xlabelsUL);						
+						var label = thisLi.find('span:not(.line)');
+						var leftOffset = label.width()/-2;
+						if(i == 0){ leftOffset = 0; }
+						else if(i== xLabels.length-1){ leftOffset = -label.width(); }
+						label
+							.css('margin-left', leftOffset)
+							.addClass('label');
+					});
+
+					//write Y labels
+					yScale = canvas.height() / totalYRange;
+					var liBottom = canvas.height() / (yLabels.length-1);
+					var ylabelsUL = $('<ul class="visualize-labels-y"></ul>')
+						.width(canvas.width())
+						.height(canvas.height())
+						.insertBefore(canvas);
+
+					$.each(yLabels, function(i){  
+						var thisLi = $('<li><span>'+this+'</span></li>')
+							.prepend('<span class="line"  />')
+							.css('bottom',liBottom*i)
+							.prependTo(ylabelsUL);
+						var label = thisLi.find('span:not(.line)');
+						var topOffset = label.height()/-2;
+						if(i == 0){ topOffset = -label.height(); }
+						else if(i== yLabels.length-1){ topOffset = 0; }
+						label
+							.css('margin-top', topOffset)
+							.addClass('label');
+					});
+					
+					charts.line.draw(area);
+
+				},
+				
+				draw: function(area) {
+					var drawPoint = function (ctx,x,y,color,size) {
+						ctx.moveTo(x,y);
+						ctx.beginPath();
+						ctx.arc(x,y,size/2,0,2*Math.PI,false);
+						ctx.closePath();
+						ctx.fillStyle = color;
+						ctx.fill();
+					}
+					var pointQueue = [];
+					var counterPoints = 0;
+					var keyPoint = function(x,y,color,myInfo) {
+						var size = o.lineWeight*Math.PI;
+						pointQueue.push(function() {
+							drawPoint(ctx,x,y,color,size);
+							if(o.lineDots === 'double') {
+								drawPoint(ctx,x,y,o.dotInnerColor,size-o.lineWeight*Math.PI/2);
+							}
+							if(o.interaction) {
+								counterPoints++;
+								interactionPoints.push({tableCords:myInfo,canvasCords:[x,y]});
+							}
+						});
+					};
+
+					//start from the bottom left
+					ctx.translate(0,zeroLoc);
+					//iterate and draw
+					$.each(dataGroups,function(h){
+						ctx.beginPath();
+						ctx.lineWidth = o.lineWeight;
+						ctx.lineJoin = 'round';
+						var points = this.points;
+						var integer = 0;
+						var myInfo = [0,0];
+						var color = this.color;
+						ctx.moveTo(0,-(points[0]*yScale));
+						$.each(points, function(g){
+							myInfo = [h,g];
+							if(o.lineDots) {
+								keyPoint(integer,-(this*yScale),color,myInfo);
+							}
+							ctx.lineTo(integer,-(this*yScale));
+							integer+=xInterval;
+						});
+						ctx.strokeStyle = color;
+						ctx.stroke();
+						if(area){
+							ctx.lineTo(integer,0);
+							ctx.lineTo(0,0);
+							ctx.closePath();
+							ctx.fillStyle = color;
+							ctx.globalAlpha = .3;
+							ctx.fill();
+							ctx.globalAlpha = 1.0;
+						}
+						else {ctx.closePath();}
+						$.each(pointQueue,function(){
+							pointQueue.shift().call();
+						});
+					});
+				}
+			};
+		
+		})();
+		
+		//function to create a chart
+		var createChart = {
+			pie: function(){
+				charts.pie.setup();
 			},
 			
 			line: function(area){
-			
-				if(area){ canvasContain.addClass('visualize-area'); }
-				else{ canvasContain.addClass('visualize-line'); }
-			
-				//write X labels
-				var xInterval = canvas.width() / (xLabels.length -1);
-				var xlabelsUL = $('<ul class="visualize-labels-x"></ul>')
-					.width(canvas.width())
-					.height(canvas.height())
-					.insertBefore(canvas);
-				$.each(xLabels, function(i){ 
-					var thisLi = $('<li><span>'+this+'</span></li>')
-						.prepend('<span class="line" />')
-						.css('left', xInterval * i)
-						.appendTo(xlabelsUL);						
-					var label = thisLi.find('span:not(.line)');
-					var leftOffset = label.width()/-2;
-					if(i == 0){ leftOffset = 0; }
-					else if(i== xLabels.length-1){ leftOffset = -label.width(); }
-					label
-						.css('margin-left', leftOffset)
-						.addClass('label');
-				});
-
-				//write Y labels
-				var yScale = canvas.height() / totalYRange;
-				var liBottom = canvas.height() / (yLabels.length-1);
-				var ylabelsUL = $('<ul class="visualize-labels-y"></ul>')
-					.width(canvas.width())
-					.height(canvas.height())
-					.insertBefore(canvas);
-					
-				$.each(yLabels, function(i){  
-					var thisLi = $('<li><span>'+this+'</span></li>')
-						.prepend('<span class="line"  />')
-						.css('bottom',liBottom*i)
-						.prependTo(ylabelsUL);
-					var label = thisLi.find('span:not(.line)');
-					var topOffset = label.height()/-2;
-					if(i == 0){ topOffset = -label.height(); }
-					else if(i== yLabels.length-1){ topOffset = 0; }
-					label
-						.css('margin-top', topOffset)
-						.addClass('label');
-				});
-				
-				var drawPoint = function (ctx,x,y,color,size) {
-					ctx.moveTo(x,y);
-					ctx.beginPath();
-					ctx.arc(x,y,size/2,0,2*Math.PI,false);
-					ctx.closePath();
-					ctx.fillStyle = color;
-					ctx.fill();
-				}
-				var pointQueue = [];
-				var counterPoints = 0;
-				var keyPoint = function(x,y,color,myInfo) {
-					var size = o.lineWeight*Math.PI;
-					pointQueue.push(function() {
-						drawPoint(ctx,x,y,color,size);
-						if(o.lineDots === 'double') {
-							drawPoint(ctx,x,y,o.dotInnerColor,size-o.lineWeight*Math.PI/2);
-						}
-						if(o.interaction) {
-							counterPoints++;
-							interactionPoints.push({tableCords:myInfo,canvasCords:[x,y]});
-						}
-					});
-				};
-
-				//start from the bottom left
-				ctx.translate(0,zeroLoc);
-				//iterate and draw
-				$.each(dataGroups,function(h){
-					ctx.beginPath();
-					ctx.lineWidth = o.lineWeight;
-					ctx.lineJoin = 'round';
-					var points = this.points;
-					var integer = 0;
-					var myInfo = [0,0];
-					var color = this.color;
-					ctx.moveTo(0,-(points[0]*yScale));
-					$.each(points, function(g){
-						myInfo = [h,g];
-						if(o.lineDots) {
-							keyPoint(integer,-(this*yScale),color,myInfo);
-						}
-						ctx.lineTo(integer,-(this*yScale));
-						integer+=xInterval;
-					});
-					ctx.strokeStyle = color;
-					ctx.stroke();
-					if(area){
-						ctx.lineTo(integer,0);
-						ctx.lineTo(0,0);
-						ctx.closePath();
-						ctx.fillStyle = color;
-						ctx.globalAlpha = .3;
-						ctx.fill();
-						ctx.globalAlpha = 1.0;
-					}
-					else {ctx.closePath();}
-					$.each(pointQueue,function(){
-						pointQueue.shift().call();
-					});
-				});
+				charts.line.setup(area);
 			},
 			
 			area: function(){
@@ -548,7 +587,7 @@ $.fn.visualize = function(options, container){
 		//append key
 		if(o.appendKey){
 			var newKey = $('<ul class="visualize-key"></ul>');
-			var selector = (o.parseDirection == 'x') ? 'tr:gt(0) th' : 'tr:eq(0) th' ;
+			var selector = (o.parseDirection == 'x') ? 'tbody tr th:first-child' : 'thead tr:eq(0) th' ;
 			self.find(selector).each(function(i){
 				$('<li><span class="visualize-key-color" style="background: '+dataGroups[i].color+'"></span><span class="visualize-key-label">'+ $(this).text() +'</span></li>')
 					.appendTo(newKey);
@@ -571,12 +610,12 @@ $.fn.visualize = function(options, container){
 				.appendTo(canvasContain);
 			var interactionPoints = [];
 			var triggerInteraction = function(overOut,cords) {
-				var selector = (o.parseDirection == 'x') ? 'tr:gt(0) th:eq('+cords[0]+')' : 'tr:eq(0) th:eq('+cords[0]+')' ;
+				var selector = (o.parseDirection == 'x') ? 'tbody tr th:eq('+cords[0]+')' : 'thead tr:eq(0) th:eq('+cords[0]+')' ;
 				var zLabel = self.find(selector).text();
 				if(o.type == 'pie') {
-					selector = (o.parseDirection == 'x') ? 'tr:eq('+(cords[0]+1)+')' : 'tr th:eq('+cords[0]+')' ;
+					selector = (o.parseDirection == 'x') ? 'tbody tr:eq('+(cords[0])+')' : 'tbody tr td:eq('+cords[0]+')' ;
 				} else {
-					selector = (o.parseDirection == 'x') ? 'tr:gt('+cords[0]+') td:eq('+cords[1]+')' : 'tr:eq(0) th:eq('+cords[0]+')' ;
+					selector = (o.parseDirection == 'x') ? 'tbody tr:eq('+cords[0]+') td:eq('+cords[1]+')' : 'tbody tr:eq('+cords[1]+') td:eq('+cords[0]+')' ;
 				}
 				var elem = self.find(selector);
 				var value = 0;
@@ -605,7 +644,7 @@ $.fn.visualize = function(options, container){
 				y = ev.layerY || ev.offsetY || 0;
 
 				found = false;
-				minDist = started?30000:(o.type=='pie'?(Math.round(canvas.height()/2)-o.pieMargin)/3:o.lineWeight*3);
+				minDist = started?30000:(o.type=='pie'?(Math.round(canvas.height()/2)-o.pieMargin)/3:o.lineWeight*4);
 				for(i=0;i<interactionPoints.length;i+=1) {
 					var current = interactionPoints[i];
 					x1 = current.canvasCords[0];
