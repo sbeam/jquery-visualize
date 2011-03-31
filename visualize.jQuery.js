e015442e8c5d386d65beabcd6cf39f36363334f1
Sam Beam <sbeam@onsetcorps.net>
2011-03-31 09:20:46 -0400

    add haslabel class to x-axis vertical lines to allow emphasis of labeled intervals

--- visualize.jQuery.js
+++ visualize.jQuery.js
@@ -307,12 +307,17 @@ $.fn.visualize = function(options, container){
 					if(!o.customXLabels) {
 						xInterval = (canvas.width() - 2*o.lineMargin) / (xLabels.length -1);
 						$.each(xLabels, function(i){ 
+							var lineClass = 'line';
+							if (this != '') lineClass += ' haslabel';
+
 							var thisLi = $('<li><span>'+this+'</span></li>')
-								.prepend('<span class="line" />')
+								.prepend('<span class="'+lineClass+'" />')
 								.css('left', o.lineMargin + xInterval * i)
 								.appendTo(xlabelsUL);						
+
 							var label = thisLi.find('span:not(.line)');
 							var leftOffset = label.width()/-2;
+
 							if(i == 0){ leftOffset = 0; }
 							else if(i== xLabels.length-1){ leftOffset = -label.width(); }
 							label
